75cef9f905f0479bf15eec8d2a207ff469f8f156
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2024-03-16 21:56:22 +0100

    add model definitions

---
+++ src/models/review.model.ts
@@ -0,0 +1,38 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { User } from './user.model';
+import { Movie } from './movie.model';
+
+@Entity()
+class Review extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  comment: string;
+
+  @Column()
+  rating: number;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @ManyToOne(() => Movie, (movie) => movie.reviews)
+  movie: Movie;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+export { Review };
